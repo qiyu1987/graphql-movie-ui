94754f7ac495c7b5b49f01c99708f532b7ba66b8
qiyu1987 <qiyu1987@gmail.com>
2019-11-10 13:28:09 +0100

    mutation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react"
 import "./App.css"
 import ApolloClient, { gql } from "apollo-boost"
-import { ApolloProvider, Query } from "react-apollo"
+import { ApolloProvider, Query, Mutation } from "react-apollo"
 
 const client = new ApolloClient({
 	uri: "http://localhost:4000/graphql"
@@ -17,25 +17,82 @@ const MOVIES_QUERY = gql`
 	}
 `
 
+const RATED_MOVIES_QUERY = gql`
+	{
+		ratedMovies {
+			id
+			title
+			rating
+		}
+	}
+`
+
+const RATE_MOVIE_MUTATION = gql`
+	mutation rateMovie($id: ID!, $rating: Int!) {
+		rateMovie(id: $id, rating: $rating)
+	}
+`
+
 class App extends Component {
 	render() {
 		return (
 			<ApolloProvider client={client}>
 				<div className="App">
-					<Query query={MOVIES_QUERY}>
-						{({ loading, error, data }) => {
-							if (loading) return <p>Loading...</p>
-							if (error) return <p>Error :(</p>
-							return this.renderMovies(data.movies)
-						}}
-					</Query>
+					<Mutation
+						mutation={RATE_MOVIE_MUTATION}
+						refetchQueries={[{ query: RATED_MOVIES_QUERY }]}
+					>
+						{rateMovie => (
+							<div style={{ float: "left", width: "50%" }}>
+								<Query query={MOVIES_QUERY}>
+									{({ loading, error, data }) => {
+										if (loading) return <p>Loading...</p>
+										if (error) return <p>Error :(</p>
+										return this.renderMovies(data.movies, rateMovie)
+									}}
+								</Query>
+							</div>
+						)}
+					</Mutation>
+					<div style={{ float: "left", width: "50%" }}>
+						<Query query={RATED_MOVIES_QUERY}>
+							{({ loading, error, data }) => {
+								if (loading) return <p>Loading...</p>
+								if (error) return <p>Error :(</p>
+								return this.renderRatedMovies(data.ratedMovies)
+							}}
+						</Query>
+					</div>
 				</div>
 			</ApolloProvider>
 		)
 	}
 
-	renderMovies(movies) {
-		return movies.map(movie => <div key={movie.id}>{movie.title}</div>)
+	renderRatedMovies(movies) {
+		return movies.map(movie => (
+			<div key={movie.id}>
+				{movie.title} - Rated at: {movie.rating}
+			</div>
+		))
+	}
+
+	renderMovies(movies, rateMovie) {
+		return movies.map(movie => (
+			<div key={movie.id}>
+				{movie.title}{" "}
+				<input
+					type="range"
+					min="0"
+					max="10"
+					value={movie.rating}
+					onChange={e =>
+						rateMovie({
+							variables: { id: movie.id, rating: parseInt(e.target.value) }
+						})
+					}
+				/>
+			</div>
+		))
 	}
 }
 
